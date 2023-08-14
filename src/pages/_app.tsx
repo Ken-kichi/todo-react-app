f7936bcd8548d20a394a90bac0258296bbab5083
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2023-08-14 13:10:27 +0900

    Authを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { useRouter } from 'next/router'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { supabase } from '../../utils/supabase'
+import { useEffect } from 'react'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   switch (metric.name) {
@@ -24,6 +30,40 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
   }
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/dashboard')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
